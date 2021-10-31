b22bb383105ae845299aa62dfdc09e5314d3bee2
Tetiana Fedorenko <1031213@gmail.com>
2021-10-31 17:08:18 +0200

    done

--- js/enemyAttack.js
+++ js/enemyAttack.js
@@ -9,4 +9,57 @@ export const HIT = {
 }
 
 
-//создание атаки врага
+
+/*
+export const getAttack = async(h, d) => {
+    
+await fetch('http://reactmarathon-api.herokuapp.com/api/mk/player/fight', {
+    method: 'POST',
+            body: JSON.stringify({
+                hit:h,
+                defence:d
+}) })
+.then((res) => {
+    return res.json()})
+    .then(data => {
+  
+    console.log(data.player2)
+    
+            return data.player2dat})
+};
+
+const a= getAttack(HIT[2],ATTACK[1])
+console.log(a)
+
+async function fetchExam(h,d) {
+    try {
+        const response = await fetch('http://reactmarathon-api.herokuapp.com/api/mk/player/fight', {
+            method: 'POST',
+            body: JSON.stringify({
+                hit:h,
+                defence:d
+            })
+        });
+      const  exam = await response.json();
+        
+        console.log(exam);
+        return exam;
+    } catch (error) {
+        console.error(error);
+    }
+}
+const ab = fetchExam(HIT[2],ATTACK[1])
+
+export const EAttack = async(h, d) =>{
+ const body = await fetch('http://reactmarathon-api.herokuapp.com/api/mk/player/fight',{
+    method: 'POST',
+    body: JSON.stringify({
+        hit:h,
+        defence:d
+    })
+})
+const res = await body.json();
+console.log(res);
+return res
+}
+EAttack(HIT[2],ATTACK[1]);*/
